a0079984ca3d8bf15e1f6c1b4a662c7c296d620a
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-02 21:38:28 -0600

    fixed text fields/database updates now

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex');
+var knex = require('knex')(require('../knexfile')['development']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,25 +8,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/addstrain', function(req, res, next) {
-        knex('strains')
-          .insert(req.body)
-          .then(function(response) {
-            res.redirect('/');
-          })
-      });
-      router.post('/addtype', function(req, res, next) {
-        knex('types')
-          .insert(req.body)
-          .then(function(response) {
-            res.redirect('/');
-          })
-      });
-      router.post('/addplace', function(req, res, next) {
-        knex('places')
-          .insert(req.body)
-          .then(function(response) {
-            res.redirect('/');
-          })
-      });
+  knex('strains')
+    .insert(req.body)
+    .then(function(response) {
+      res.redirect('/');
+    })
+});
+
+router.post('/addtype', function(req, res, next) {
+  knex('types')
+    .insert(req.body)
+    .then(function(response) {
+      res.redirect('/');
+    })
+});
+
+router.post('/addplace', function(req, res, next) {
+  knex('places')
+    .insert(req.body)
+    .then(function(response) {
+      res.redirect('/');
+    })
+});
 
 module.exports = router;
