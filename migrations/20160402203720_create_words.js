0e3bf344569275fe2fc8e92d54b44f3d7c80378a
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-02 20:44:14 -0600

    created migrations/migrate:latest

--- migrations/20160402203720_create_words.js
+++ migrations/20160402203720_create_words.js
@@ -1,8 +1,35 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+
+    knex.schema.createTable('users', function( table ){
+      table.increments();
+      table.string('userName');
+      table.string('password');
+    }),
+    knex.schema.createTable('strains', function( table ){
+      table.increments();
+      table.string('word');
+    }),
+    knex.schema.createTable('types', function( table ){
+      table.increments();
+      table.string('word');
+    }),
+    knex.schema.createTable('places', function( table ){
+      table.increments();
+      table.string('word');
+    })
+
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('strains'),
+    knex.schema.dropTable('types'),
+    knex.schema.dropTable('places')
+
+  ])
 };
