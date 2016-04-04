a52b11d79374db63b801ea6c51fb602271729757
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-04 16:37:59 -0600

    adding login functionality/not getting log-in inputs to show

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/user/register', function(req, res, next) {
+  var hash = bcrypt.hashSync(req.body.password, 8);
+  knex('users')
+    .insert({
+      'userName': req.body.username.toLowerCase(),
+      'password': hash
+    })
+    .then(function(response) {
+      res.redirect('/');
+    })
+});
+
+router.post('/user/login', function(req, res, next) {
+  knex('users')
+    .where('userName', '=', req.body.username.toLowerCase())
+    .first()
+    .then(function(response) {
+      if (response && bcrypt.compareSync(req.body.password, response.password)) {
+        res.redirect('/home/' + response.userName);
+      } else {
+        res.render('auth', {
+          error: 'Invalid username or password'
+        });
+      }
+    });
+});
+
 module.exports = router;
